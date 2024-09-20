c7ec54a64151365f061fdb2cab57c023639fb558
huseynstudent <huseynstudent5628@gmail.com>
2024-09-20 22:21:24 +0400

    display boards done

--- ship_battle.cpp
+++ ship_battle.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
-
 using namespace std;
-
 const int SIZE = 10;
-char playerBoard[SIZE][SIZE];
-char opponentBoard[SIZE][SIZE];
-void initializeBoard(char board[SIZE][SIZE]) {
+char playerboard[SIZE][SIZE];
+char opponentboard[SIZE][SIZE];
+void initializeboard(char board[SIZE][SIZE]);
+void displayboard(char board[SIZE][SIZE]);
+int main() {
+    initializeboard(playerboard);
+    displayboard(playerboard);
+    initializeboard(opponentboard);
+    displayboard(opponentboard);
+
+
+    
+}
+
+
+
+
+void initializeboard(char board[SIZE][SIZE]) {
     for (int i = 0; i < SIZE; i++)
         for (int j = 0; j < SIZE; j++)
             board[i][j] = '~';  
 }
-
-void displayBoard(char board[SIZE][SIZE]) {
+void displayboard(char board[SIZE][SIZE]) {
     cout << "  0 1 2 3 4 5 6 7 8 9\n";
     for (int i = 0; i < SIZE; i++) {
         cout << i << " ";
@@ -21,8 +33,4 @@ void displayBoard(char board[SIZE][SIZE]) {
         cout << endl;
     }
 }
-
-int main() {
-    
-
-}
+   
