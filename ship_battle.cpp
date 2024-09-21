be8aa25c417ed5d5c0eeef39ef5b832e9b5ff1b9
huseynstudent <huseynstudent5628@gmail.com>
2024-09-21 16:26:33 +0400

    ship placement (with problems)done

--- ship_battle.cpp
+++ ship_battle.cpp
@@ -5,14 +5,23 @@ char playerboard[SIZE][SIZE];
 char opponentboard[SIZE][SIZE];
 void initializeboard(char board[SIZE][SIZE]);
 void displayboard(char board[SIZE][SIZE]);
+void placeship(char board[SIZE][SIZE], int length);
 int main() {
     initializeboard(playerboard);
     displayboard(playerboard);
+    cout << endl << endl;
     initializeboard(opponentboard);
     displayboard(opponentboard);
-
-
-    
+    int shiplength = 4;
+    placeship(playerboard, shiplength);
+    shiplength--;
+    placeship(playerboard, shiplength);
+    shiplength--;
+    placeship(playerboard, shiplength);
+    placeship(playerboard, shiplength);
+    shiplength--;
+    placeship(playerboard, shiplength);
+    placeship(playerboard, shiplength);
 }
 
 
@@ -33,4 +42,56 @@ void displayboard(char board[SIZE][SIZE]) {
         cout << endl;
     }
 }
-   
+void placeship(char board[SIZE][SIZE], int length) {
+    int x, y;
+    char direction;
+    cout << "Placing a ship of length " << length << ".\n";
+    cout << "Enter starting coordinates (row and column): ";
+    cin >> x >> y;
+    if (length==1)
+        board[x][y] = '@';
+    else {
+        cout << "Enter direction (h for horizontal, v for vertical): ";
+        cin >> direction;
+        if (direction == 'h') {
+            if (y + length > SIZE) {
+                cout << "Ship does not fit horizontally. Try again.\n";
+                placeship(board, length);
+                return;
+            }
+            for (int i = 0; i < length; i++) {
+                if (board[x][y + i] != '~') {
+                    cout << "Overlap detected. Try again.\n";
+                    placeship(board, length);
+                    return;
+                }
+            }
+            for (int i = 0; i < length; i++) {
+                board[x][y + i] = '@';
+            }
+        }
+        else if (direction == 'v') {
+            if (x + length > SIZE) {
+                cout << "Ship does not fit vertically. Try again.\n";
+                placeship(board, length);
+                return;
+            }
+            for (int i = 0; i < length; i++) {
+                if (board[x + i][y] != '~') {
+                    cout << "Overlap detected. Try again.\n";
+                    placeship(board, length);
+                    return;
+                }
+            }
+            for (int i = 0; i < length; i++) {
+                board[x + i][y] = '@';
+            }
+        }
+        else {
+            cout << "Invalid direction. Please enter 'h' or 'v'.\n";
+            placeship(board, length);
+            return;
+        }
+    }
+    displayboard(board);
+}
