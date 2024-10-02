e8a63c5bb9196864f01a2aa7adb34246c5109449
huseynstudent <huseynstudent5628@gmail.com>
2024-10-03 00:14:48 +0400

    basic attackplayer done with problems

--- ship_battle.cpp
+++ ship_battle.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream> 
 #include <conio.h>
 #pragma warning(disable:4996);
 
@@ -7,9 +7,8 @@ const int SIZE = 10;
 const char sea = '~';
 const char ship = '@';
 const char block = 'X';
-const char wrecked_ship = '#'; // player's
 const char missed = 'M';
-const char hit_ship = 'O';     // opponent's
+const char hit_ship = 'O';
 
 char playerboard[SIZE][SIZE];
 char opponentboard[SIZE][SIZE];
@@ -25,7 +24,7 @@ void displayboard(char board[SIZE][SIZE]);
 void placeship(char board[SIZE][SIZE], int length, char hidden[SIZE][SIZE]);
 void markblocks(char board[SIZE][SIZE], int x, int y, int length, char direction);
 void markblock1(char board[SIZE][SIZE], int x, int y);
-void attackplayer(char attackboard[SIZE][SIZE], char hidden[SIZE][SIZE],int count);
+void attackplayer(char attackboard[SIZE][SIZE], char hidden[SIZE][SIZE], int& count);
 
 int main() {
 	char enter;
@@ -52,7 +51,7 @@ int main() {
 	placeship(playerboard, shiplength, hiddenboard_player);
 	cout << "\x1B[2J\x1B[H";
 	cout << "give keyboard to player 2 and press enter afterwards";
-	enter = _getch();//just to make sure user read the instruction,has no use
+	enter = _getch(); //just to make sure user read the instruction, has no use
 	cout << endl;
 	displayboard(opponentboard);
 	cout << "\nplayer 2 places ships:\n";
@@ -64,32 +63,32 @@ int main() {
 	placeship(opponentboard, shiplength, hiddenboard_opponent);
 	placeship(opponentboard, shiplength, hiddenboard_opponent);
 	cout << "\x1B[2J\x1B[H";
-	cout << "both players have 6 ships(14 pieces).first to eliminate all of other's wins\n\npress enter to roll the dice(odd-player1,even-player 2 starts)";
+	cout << "both players have 6 ships(14 pieces).first to eliminate all of other's wins.use arrows and space to navigate\n\npress enter to roll the dice(odd-player1,even-player 2 starts)";
 	enter = _getch();
 	int dice = dice_roll();
 	cout << "dice rolled and result is:" << dice;
 	if (dice % 2 == 0) {
 		cout << "\n\teven,player 2 starts\n";
-		attackplayer(attackboard_opponent,hiddenboard_player,players_ship_count);
+		attackplayer(attackboard_opponent, hiddenboard_player, players_ship_count);
 	}
 	else cout << "\n\todd,player 1 starts";
 	while (players_ship_count > 0 && opponents_ship_count > 0) {
-		attackplayer(attackboard_player, hiddenboard_opponent,opponents_ship_count);
-		attackplayer(attackboard_opponent,hiddenboard_player,players_ship_count);
+		cout << "\n\tplayer1:\n";
+		attackplayer(attackboard_player, hiddenboard_opponent, opponents_ship_count);
+		cout << "\n\tplayer2:\n";
+		attackplayer(attackboard_opponent, hiddenboard_player, players_ship_count);
 	}
 	if (players_ship_count == 0)
 		displaywinner(2);
 	else displaywinner(1);
 }
 
-
-
-
 void initializeboard(char board[SIZE][SIZE]) {
 	for (int i = 0; i < SIZE; i++)
 		for (int j = 0; j < SIZE; j++)
 			board[i][j] = '~';
 }
+
 void displayboard(char board[SIZE][SIZE]) {
 	cout << "  0 1 2 3 4 5 6 7 8 9\n";
 	for (int i = 0; i < SIZE; i++) {
@@ -101,6 +100,7 @@ void displayboard(char board[SIZE][SIZE]) {
 	}
 	cout << endl;
 }
+
 void markblocks(char board[SIZE][SIZE], int x, int y, int length, char direction) {
 	for (int i = -1; i <= 1; i++) {
 		for (int j = -1; j <= length; j++) {
@@ -114,6 +114,7 @@ void markblocks(char board[SIZE][SIZE], int x, int y, int length, char direction
 		}
 	}
 }
+
 void markblock1(char board[SIZE][SIZE], int x, int y) {
 	for (int i = -1; i <= 1; i++) {
 		for (int j = -1; j <= 1; j++) {
@@ -127,10 +128,12 @@ void markblock1(char board[SIZE][SIZE], int x, int y) {
 		}
 	}
 }
+
 int dice_roll() {
-	int dice = 1+rand() % 6;
+	int dice = 1 + rand() % 6;
 	return dice;
 }
+
 void displaywinner(int player) {
 	cout << "\x1B[2J\x1B[H";
 	if (player == 1) {
@@ -140,15 +143,16 @@ void displaywinner(int player) {
 		cout << "\t****************************\n";
 	}
 	else if (player == 2) {
-		cout << "\033[31m";//red
+		cout << "\033[31m"; //red
 		cout << "\t****************************\n";
 		cout << "\t*     PLAYER 2 WINS!       *\n";
 		cout << "\t****************************\n";
 	}
-	cout << "\033[33m"; 
+	cout << "\033[33m";
 	cout << "thanks for playing,this was all";
 	cout << "\033[0m";
 }
+
 void placeship(char board[SIZE][SIZE], int length, char hidden[SIZE][SIZE]) {
 	int x; int y; char confirm;
 	char direction;
@@ -157,105 +161,147 @@ void placeship(char board[SIZE][SIZE], int length, char hidden[SIZE][SIZE]) {
 	cin >> x;
 	cout << "\nEnter starting coordinates (column): ";
 	cin >> y;
-
 	cout << endl;
-	if (length == 1) {
-		for (int i = 0; i < length; i++) {
-			if (board[x][y] != '~') {
-				cout << "Overlap or blocked area detected. Try again.\n\n";
-				placeship(board, length, hidden);
-				return;
-			}
-		}
-		hidden[x][y] = ship;
-		displayboard(hidden);
-		cout << "\ncurrent ship placement will be like this.\tconfirm(c is confirm and rest is not confirm)";
-		cin >> confirm;
-		if (confirm == 'c' || confirm == 'C') {
-			board[x][y] = ship;
-			markblock1(board, x, y);
-		}
-		else {
-			hidden[x][y] = sea;
-			placeship(board, length, hidden);
-			return;
-		}
-	}
-	else {
-		cout << "Enter direction (h for horizontal, v for vertical): ";
-		cin >> direction;
-		if (direction == 'h') {
-			if (y + length > SIZE) {
-				cout << "Ship does not fit horizontally. Try again.\n\n";
-				placeship(board, length, hidden);
-				return;
-			}
+	if (x >= 0 && x <= 9 || y >= 0 && y <= 9) {
+		if (length == 1) {
 			for (int i = 0; i < length; i++) {
-				if (board[x][y + i] != '~') {
+				if (board[x][y] != '~') {
 					cout << "Overlap or blocked area detected. Try again.\n\n";
 					placeship(board, length, hidden);
 					return;
 				}
 			}
-			for (int i = 0; i < length; i++) {
-				hidden[x][y + i] = ship;
-			}
-			displayboard(hidden);
-			cout << "\ncurrent ship placement will be like this.\tconfirm(c is confirm and rest is not confirm)";
-			cin >> confirm;
-			if (confirm == 'c' || confirm == 'C') {
-				for (int i = 0; i < length; i++) {
-					board[x][y + i] = ship;
-				}
-				markblocks(board, x, y, length, 'h');
-			}
-			else {
-				for (int i = 0; i < length; i++) {
-					hidden[x][y + i] = sea;
-				}
-				cout << "\n\tconfirm denied\n";
-				placeship(board, length, hidden);
-			}
+			board[x][y] = ship;
+			markblock1(board, x, y);
+			displayboard(board);
+			return;
 		}
-		else if (direction == 'v') {
-			if (x + length > SIZE) {
+		else {
+			cout << "Enter direction (h for horizontal, v for vertical): ";
+			cin >> direction;
+			if (direction == 'h') {
+				if (y + length > SIZE) {
+					cout << "Ship does not fit horizontally. Try again.\n\n";
+					placeship(board, length, hidden);
+					return;
+				}
+				for (int i = 0; i < length; i++) {
+					if (board[x][y + i] != '~') {
+						cout << "Overlap or blocked area detected. Try again.\n\n";
+						placeship(board, length, hidden);
+						return;
+					}
+				}
+				for (int i = 0; i < length; i++) {
+					hidden[x][y + i] = ship;
+				}
+				displayboard(hidden);
+				cout << "\ncurrent ship placement will be like this.\tconfirm(c is confirm and rest is not confirm)";
+				cin >> confirm;
+				if (confirm == 'c' || confirm == 'C') {
+					for (int i = 0; i < length; i++) {
+						board[x][y + i] = ship;
+					}
+					markblocks(board, x, y, length, direction);
+				}
+				else {
+					for (int i = 0; i < length; i++) {
+						hidden[x][y + i] = sea;
+					}
+					placeship(board, length, hidden);
+					return;
+				}
+			}
+			else if (direction == 'v') {
+				if (x + length > SIZE) {
 					cout << "Ship does not fit vertically. Try again.\n\n";
 					placeship(board, length, hidden);
 					return;
-			}
-			for (int i = 0; i < length; i++) {
+				}
+				for (int i = 0; i < length; i++) {
 					if (board[x + i][y] != '~') {
 						cout << "Overlap or blocked area detected. Try again.\n\n";
 						placeship(board, length, hidden);
 						return;
 					}
-			}
-			for (int i = 0; i < length; i++) {
-				hidden[x+i][y] = ship;
-			}
-			displayboard(hidden);
-			cout << "\ncurrent ship placement will be like this.\tconfirm(c is confirm and rest is not confirm)";
-			cin >> confirm;
-			if (confirm == 'c' || confirm == 'C') {
-				for (int i = 0; i < length; i++) {
-					board[x+i][y] = ship;
 				}
-				markblocks(board, x, y, length, 'v');
+				for (int i = 0; i < length; i++) {
+					hidden[x + i][y] = ship;
+				}
+				displayboard(hidden);
+				cout << "\ncurrent ship placement will be like this.\tconfirm(c is confirm and rest is not confirm)";
+				cin >> confirm;
+				if (confirm == 'c' || confirm == 'C') {
+					for (int i = 0; i < length; i++) {
+						board[x + i][y] = ship;
+					}
+					markblocks(board, x, y, length, direction);
+				}
+				else {
+					for (int i = 0; i < length; i++) {
+						hidden[x + i][y] = sea;
+					}
+					placeship(board, length, hidden);
+					return;
+				}
 			}
 			else {
-				for (int i = 0; i < length; i++) {
-					hidden[x+i][y] = sea;
-				}
-				cout << "\n\tconfirm denied\n";
+				cout << "Invalid direction. Try again.\n\n";
 				placeship(board, length, hidden);
+				return;
 			}
 		}
-		else {
-			cout << "Invalid direction. Please enter 'h' or 'v'.\n\n";
-			placeship(board, length, hidden);
-		}
+	}
+	else {
+		cout << "Invalid coordinates. Please enter a number(0-9)\n\n";
+		placeship(board, length, hidden);
+		return;
 	}
 }
-void attackplayer(char attackboard[SIZE][SIZE], char hidden[SIZE][SIZE], int count) {
-	cout << 'a';
+
+void attackplayer(char attackboard[SIZE][SIZE], char hidden[SIZE][SIZE], int& count) {
+	int cursorX = 0; 
+	int cursorY = 0; 
+	char input;
+	bool validMove = false;
+
+	while (!validMove) {
+		cout << "\x1B[2J\x1B[H"; 
+		displayboard(attackboard); 
+		attackboard[cursorX][cursorY] = '*'; 
+		cout << "\nUse arrow keys to move the cursor. Press space to attack.\n";
+
+		input = _getch(); 
+
+		switch (input) {
+		case 72: // Up arrow
+			if (cursorX > 0) cursorX--;
+			break;
+		case 80: // Down arrow
+			if (cursorX < SIZE - 1) cursorX++;
+			break;
+		case 75: // Left arrow
+			if (cursorY > 0) cursorY--;
+			break;
+		case 77: // Right arrow
+			if (cursorY < SIZE - 1) cursorY++;
+			break;
+		case 32: // Space
+			if (hidden[cursorX][cursorY] == ship) {
+				cout << "Hit!\n";
+				hidden[cursorX][cursorY] = sea; 
+				attackboard[cursorX][cursorY] = hit_ship; 
+				count--; 
+			}
+			else {
+				cout << "Miss!\n";
+				attackboard[cursorX][cursorY] = missed; 
+			}
+			validMove = true; 
+			break;
+		default:
+			break; 
+		}
+		attackboard[cursorX][cursorY] = ' '; 
+	}
 }
